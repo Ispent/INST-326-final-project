9eac24f9f5d8188be6aa7ea5469c97688fbd03dc
patty <seunghak2003@gmail.com>
2025-04-28 15:12:38 -0400

    wrote up player-betting functions, need to write tests still

---
+++ player.py
@@ -0,0 +1,43 @@
+
+class Player:
+    def __init__(self, name, is_dealer=False):
+        # i guess 500 bucks is enough to start off with
+        self.name = name
+        self.balance = 500
+        self.hand = []
+        self.is_dealer = is_dealer
+
+
+
+    def bet(self, amount):
+        if self.balance < amount:
+            raise ValueError('too broke')
+        
+        self.balance -= amount
+        return amount
+    
+    def fold(self):
+        self.hand = []
+        return 'player has folded'
+
+    def check(self):
+        # i dont think i even need this ??
+        return 'check'
+    
+    def call(self, current_bet):
+        return self.bet(current_bet)
+    
+    def raise_bet(self, current_bet, raise_amount):
+        new_bet = current_bet + raise_amount
+        if self.balance < new_bet:
+            raise ValueError('broke')
+        
+        return self.bet(new_bet)
+
+
+
+    
+    
+    
+
+        
