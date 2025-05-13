93fd39962a724880ca8e5bebdeab10f7dbe163bf
Brenda Ngaba <bngaba@terpmail.edu>
2025-05-13 19:24:26 -0400

    removed files accidentally added

--- car_test.py
+++
@@ -1,5 +0,0 @@
-heading = int(270)
-degrees = float(100)
-heading += degrees
-heading %= 360
-print (heading)
--- slots.py
+++ slots.py
@@ -1,11 +1,11 @@
 import random
 import time
-
+import sys
 
 class SlotMachine:
     """
     """
-    def __init__(self, starting_points):
+    def __init__(self, starting_points): #will replace starting points with player
         self.symbols = ['🍒', '🍋', '🔔', '💎', '🍀']
         self.symbol_value = {
             '🍒' : 20,
@@ -14,27 +14,38 @@ class SlotMachine:
             '💎' : 50, 
             '🍀' : 25
             }
+        #self.player = player
         self.points = starting_points
         self.spin_cost = 10 #can change but for now
+        print(f"🎰 Welcome to Slots 🎰")
     
     def can_play(self):
         #checks if user has enough points to play
+        #if self.player.balance >= self.spin_cost:
         if self.points >= self.spin_cost:
             return True
         else:
-            short_points =self.spin_cost - self.points
+            #short_points = self.spin_cost - self.player.balance
+            short_points = self.spin_cost - self.points
             print(f"Sorry, you need {short_points} more points to play this game.")
-            exit()
+            sys.exit() #will return to main when there is a main
 
     def spin_reels(self):
         self.points -= self.spin_cost
+        play = input(f"Would you like to play slots for $10? (y/n): \n")
+        if play not in ['y' or 'yes']:
+            print("Returning to main menu...")
+            time.sleep(.5)
+            sys.exit()
+            #will replace w return to main 
+
         print(f"You now have {self.points} points.")
         time.sleep(1)
         
         print("Spinning...")
         time.sleep(3)
         
-        for _ in range(15):  # number of animation frames
+        for _ in range(15):  #number of animation frames
             spin_animation = [random.choice(self.symbols) for _ in range(3)]
             print("\r" + " | ".join(spin_animation), end="", flush=True)
             time.sleep(0.1)
@@ -69,9 +80,9 @@ class SlotMachine:
             print(self.results(spins))
             print(f"Current points: {self.points}\n")
             
-            play_again =  input("Play again? (y/n): ").strip().lower()
+            play_again =  input("Play again? (y/n): \n").strip().lower()
             if play_again not in ['y', 'yes']:
-                print("Thanks for playing!")
+                print(f"Thanks for playing! You have {self.points} remaining.")
                 break
     
 if __name__ == "__main__":
